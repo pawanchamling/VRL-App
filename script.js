61341d307f663cef108882a5803247b72c5d9389
Pawan Chamling Rai (Kirat) <pawanchamling.kirat@gmail.com>
2015-04-22 20:46:56 +0200

    more testing: adding second data set

--- script.js
+++ script.js
@@ -16,59 +16,94 @@ myWidget = function() {
     };
 	
     var width = 960;
-    var height = 500;
+    var height = 600;
 	
     var contextHeight = 100;
-	var contextHeightPadding = 5;
+	var timelineHeight = 100;
+	var contextHeightPadding = 5;	
     var areaSpace = 40;
-
+	
+	var noOfData = 1;
+	var timelineBarHeight = 20; //timelineHeight / noOfData;
+	
+	
+	//==< for browser logs >===
+	function log(msg, color) {
+		
+		if($.browser.msie){
+			console.log(msg);
+		}
+		else {
+			console.log("%c" + msg, "color:" + color + ";font-weight:bold;");
+		}
+	}
+	function log(msg) {
+		console.log(msg);
+	}
+	//==< for browser logs >===
+	
     function widget(selection) {
         selection.each(function(data) {
 			
+			log(data);
+			data = data.values;
+			log(data);
+			
+			
             var availableWidth = width - padding.left - padding.right;
             var availableHeight = height - padding.top - padding.bottom;
-
+			
+			var focusHeight = availableHeight - contextHeight - timelineHeight ;
+			
             var container = d3.select(this);
             var svg = container.append('svg')
                 .attr('width', width + margin.left + margin.right)
                 .attr('height', height + margin.top + margin.bottom);
 						
-            var x = d3.time.scale().range([0, availableWidth]); //for focus
-            var x2 = d3.time.scale().range([0, availableWidth]); //for context
-            var y = d3.scale.linear().range([availableHeight - contextHeight - areaSpace, 0]);
-            var y2 = d3.scale.linear().range([contextHeight - contextHeightPadding, 0 + contextHeightPadding]);
+            var xFocus 	 = d3.time.scale().range([0, availableWidth]); //for focus
+            var xContext = d3.time.scale().range([0, availableWidth]); //for context
+            var xTimeline= d3.time.scale().range([0, availableWidth]); //for timeline
+            var yFocus = d3.scale.linear().range([focusHeight - areaSpace, 0]);
+            var yContext = d3.scale.linear().range([contextHeight - contextHeightPadding , 0 + contextHeightPadding]);
+            var yTimeline = d3.scale.linear().range([timelineHeight, 0 ]);
 			
-            var xAxis = d3.svg.axis().scale(x).orient('bottom');
-            var xAxis2 = d3.svg.axis().scale(x2).orient('bottom');
-            var yAxis = d3.svg.axis().scale(y).orient('left');
-            var yAxis2 = d3.svg.axis().scale(y).orient('right');
+            var xAxisFocus = d3.svg.axis().scale(xFocus).orient('bottom');
+            var xAxisContext = d3.svg.axis().scale(xContext).orient('bottom');
+            var xAxisTimeline = d3.svg.axis().scale(xTimeline).orient('bottom');
+            var yAxisFocusLeft = d3.svg.axis().scale(yFocus).orient('left');
+            var yAxisFocusRight = d3.svg.axis().scale(yFocus).orient('right');
 	
 			var xDomain = [];
 			data.forEach(function(d) {
-				//console.log(d.timestamp);
+				//log(d.timestamp);
 				xDomain.push( new Date(d.timestamp * 1000));
 			});
 			
 			
 				
 			//for focus	
-            var line = d3.svg.line()
+            var lineFocus = d3.svg.line()
                 .interpolate('monotone')
-                .x(function(d) { return x(new Date(d.timestamp * 1000)); })
-                .y(function(d) { return y(d.value - 0);});
+                .x(function(d) { return xFocus(new Date(d.timestamp * 1000)); })
+                .y(function(d) { return yFocus(d.value - 0);});
 
 			//for context
-            var line2 = d3.svg.line()
+            var lineContext = d3.svg.line()
                 .interpolate('monotone')
-                .x(function(d) { return x2(new Date(d.timestamp * 1000)); })
-                .y(function(d) { console.log("line 2 value:" + d.value);
-				return y2(d.value - 0);});
+                .x(function(d) { return xContext(new Date(d.timestamp * 1000)); })
+                .y(function(d) { return yContext(d.value - 0);});
 
+			var lineTimeline = d3.svg.line()
+                .interpolate('monotone')
+                .x(function(d) { return xTimeline(new Date(d.timestamp * 1000)); })
+                .y(function(d) { return 89;}); //where to keep the bar
+				
+				
             svg.append('defs').append('clipPath')
                 .attr('id', 'clip')
                 .append('rect')
                 .attr('width', availableWidth)
-                .attr('height', height - contextHeight - areaSpace);
+                .attr('height', height - contextHeight - timelineHeight - areaSpace);
 
             var wrap = svg.append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
@@ -77,9 +112,13 @@ myWidget = function() {
                 .attr('class', 'focus')
                 .attr('transform', 'translate(' + padding.left + ',' + padding.top + ')');
 
-            var context = wrap.append('g')
+            var timeline = wrap.append('g')
+                .attr('class', 'timeline')
+                .attr('transform', 'translate(' + padding.left + ',' + (availableHeight - timelineHeight) + ')');
+           
+		   var context = wrap.append('g')
                 .attr('class', 'context')
-                .attr('transform', 'translate(' + padding.left + ',' + (availableHeight - contextHeight) + ')');
+                .attr('transform', 'translate(' + padding.left + ',' + (focusHeight) + ')');
 
 					          
 			//var yMax = d3.max(data.map(function(d) { return d[1]; } ));
@@ -88,52 +127,70 @@ myWidget = function() {
 				yValues.push(d.value - 0); 
 			});
 			
-			//console.log(yValues);
+			//log(yValues);
 			var yMax = d3.max(yValues);
 			//var yMax = 100;
-			console.log("yMax = " + yMax);
+			log("yMax = " + yMax);
 			
-            x.domain(d3.extent(xDomain));
-            y.domain([0, yMax]);
-            x2.domain(x.domain());
-            y2.domain(y.domain());			
+            xFocus.domain(d3.extent(xDomain));	
+            xContext.domain(xFocus.domain());		
+            xTimeline.domain(xFocus.domain());
+            yFocus.domain([0, yMax]);		
+            yContext.domain(yFocus.domain());
+            yTimeline.domain(yFocus.domain());				
 			
             focus.append('path')
                 .data(data)
                 .attr('class', 'line')
-                .attr('d', line);
+                .attr('d', lineFocus);
 			
             focus.append('g')
                 .attr('class', 'x axis')
-                .attr('transform', 'translate(0,' + (availableHeight - contextHeight - areaSpace) + ')')
-                .call(xAxis);
+                .attr('transform', 'translate(0,' + (focusHeight - areaSpace) + ')')
+                .call(xAxisFocus);
 
             focus.append('g')
                 .attr('class', 'y axis')
-                .call(yAxis);
+                .call(yAxisFocusLeft);
 
             focus.append('g')
                 .attr('class', 'y axis')
                 .attr('transform', 'translate(' + availableWidth + ',0)')
-                .call(yAxis2);
+                .call(yAxisFocusRight);
+				
+				
+			timeline.append('path')
+                .data(data)
+                .attr('class', 'line')
+                .attr('d', lineTimeline)
+				.style("stroke-width", timelineBarHeight);
+				
+			timeline.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,' + (timelineHeight)  + ')')
+                .call(xAxisTimeline);
 
             context.append('path')
                 .data(data)
                 .attr('class', 'line')
-                .attr('d', line2);
+                .attr('d', lineContext);
 
             context.append('g')
                 .attr('class', 'x axis')
-                .attr('transform', 'translate(0,' + contextHeight + ')')
-                .call(xAxis2);
+                .attr('transform', 'translate(0,' + contextHeight  + ')')
+                .call(xAxisContext);
+			
+			
+				
+				
 			
 			var startDate = new Date(xDomain[0] - 0);
 			var lastDate = new Date(xDomain[xDomain.length - 1] - 0);
-			console.log("start date :" + startDate);
-			console.log("Stop date :" + lastDate);
+			log("start date :" + startDate);
+			log("Stop date :" + lastDate);
 			 
 			 var theBrush = d3.svg.brush()
-                .x(x2)
+                .x(xContext)
 				.extent([startDate, lastDate])
                 .on('brush', brushed);
        
@@ -152,30 +209,33 @@ myWidget = function() {
 				
 			context.selectAll("rect")
                 .attr('y', contextHeightPadding - 2)
-				.attr("height", contextHeight - contextHeightPadding + 1);
+				.attr("height", contextHeight + timelineHeight - contextHeightPadding + 1);
 				
 			//the handles configuration
 			  context.selectAll(".resize").append("rect")
 				.attr("width", '5px')
-				.attr("height", contextHeight + 15)
+				.attr("height", contextHeight + timelineHeight + 15)
 				.classed("border");
 		    
-								
+			//context.style("background-color", "#00ff00");					
 		
 			
 			
             function brushed() {
-                console.log("extent: " + theBrush.extent());
+                log("extent: " + theBrush.extent());
 				
-                x.domain(theBrush.empty() ? x2.domain() : theBrush.extent());				
-                focus.select('.line').attr('d', line);	//show the focus line		
-                context.select('.line').attr('d', line2);	//show the context line												
-                focus.select('.x.axis').call(xAxis);
+                xFocus.domain(theBrush.empty() ? xContext.domain() : theBrush.extent());				
+                focus.select('.line').attr('d', lineFocus);	//show the focus line		
+                timeline.select('.line').attr('d', lineTimeline);	//show the timeline line	
+                context.select('.line').attr('d', lineContext);	//show the context line													
+                focus.select('.x.axis').call(xAxisFocus);
+				
+				
             }
 			
 			function nobrush(a, b, c) {
 				//to stop the brushing from the chart background
-				console.log('Brushing from background diabled')
+				log('Brushing from background diabled')
 				d3.event.stopPropagation()
 			}
         });
@@ -226,6 +286,13 @@ myWidget = function() {
         areaSpace = _;
         return widget;
     };
-
+	
+	//##############################################
+	widget.timelineBarHeight = function(_) {
+        if (!arguments.length) return timelineBarHeight;
+        timelineBarHeight = _;
+        return widget;
+    };
+	
     return widget;
 };
